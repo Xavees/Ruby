30ec4d52a8862332e979d2d05e2a88d274fd1106
Xavees <luisfernando1337x@gmail>
2026-04-15 20:24:12 -0300

    Iniciando o repositorio

---
+++ seção1e2/imprimirNumber.rb
@@ -0,0 +1,23 @@
+puts "1 + 2";
+
+puts "#{1 + 2}";
+meuNome = "Xavees";
+idade = 20;
+puts meuNome.class;
+puts idade.class;
+
+dinheiro = 2.5
+estado = true
+
+puts dinheiro.class;
+
+
+puts "O meu nome é: #{meuNome}, minha idade é #{idade}, eu tenho #{dinheiro} de dinheiro e estado é #{estado}";
+
+# class, pra descobrir o tipo primitivo da variavel em Ruby
+# o print ( puts ) do Ruby, aparentemente é por linhas
+
+
+# quando tem uma sequencia de letras chamamos de String, como se fosse uma frase.
+# enquanto se for um caracter isolado, chamamos somente de caracter.
+# mas as linguagens de programação consideram um caracter so, como String.
