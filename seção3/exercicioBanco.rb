3104e097704784a911f83f0f88a0b5b785085ca6
Xavees <luisfernando1337x@gmail>
2026-04-20 21:29:00 -0300

    fazendo uma classe de exercicioBanco

--- seção3/exercicioBanco.rb
+++ seção3/exercicioBanco.rb
@@ -1,17 +1,35 @@
 class Conta
-  attr_writer :titular
+  attr_accessor :titular
   attr_reader :saldo
 
-def initialize (titular = 0 , saldo = 0)
-  @titular = titular
-  @saldo = saldo
-end
-
-
-def depositar (valor)
-  
-
+  def initialize(titular = "Usuario", saldo = 0)
+    @titular = titular
+    @saldo = saldo
   end
 
+  def depositar(valorDeposito)
+    if valorDeposito > 0
+      @saldo += valorDeposito
+      puts "Depósito realizado com sucesso"
+    else
+      puts "Valor inválido, adicione mais do que 0 reais"
+    end
+  end
 
-end  
+  def sacar(valorSaque)
+    if valorSaque <= 0
+      puts "Você não pode fazer saques negativos"
+    elsif valorSaque <= @saldo
+      @saldo -= valorSaque
+      puts "Saque realizado com sucesso"
+      puts "Valor sacado: #{valorSaque}"
+      puts "Saldo atual: #{@saldo}"
+    else
+      puts "Saldo insuficiente"
+    end
+  end
+
+  def ver_saldo
+    puts "Saldo atual: #{@saldo}"
+  end
+end
